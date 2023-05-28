dee833b35b35627e3bf715df1216b0a7c285a170
Katarzyna Ruszel <kat.ruszel@gmail.com>
2023-05-28 14:47:23 +0200

    Second test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -18,9 +18,25 @@ public class UltimateQATest extends PageSetUp {
     WebElement buttonSuccessText = driver.findElementByClassName( "entry-title");
     assertEquals ("Button success", buttonSuccessText.getText());
 
-
-
     }
+@Test
+    public void TestTwo() throws InterruptedException {
+        WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
+        nameTextField.sendKeys("Tester");
 
+
+        WebElement emailTextField = driver.findElementByName("et_pb_contact_email_0");
+        emailTextField.sendKeys("tester@tester.com");
+
+        Thread.sleep(3000);
+
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
+
+        Thread.sleep(3000);
+
+        WebElement thanksText =driver.findElementByClassName("et-pb-contact-message");
+        assertEquals("Thanks for contacting us", thanksText.getText());
+}
 
 }
