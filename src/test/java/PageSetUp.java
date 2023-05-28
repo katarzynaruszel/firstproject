7e57ffa5fcd72523d3a276f483d10555b9a761f2
Katarzyna Ruszel <kat.ruszel@gmail.com>
2023-05-28 12:47:26 +0200

    Project created

---
+++ src/test/java/PageSetUp.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageSetUp {
+    String projectLocation = System.getProperty("user.dir");
+    ChromeDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", projectLocation + "/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+    }
+    @AfterEach
+        public void teardown() {
+            driver.close();
+        }
+}
